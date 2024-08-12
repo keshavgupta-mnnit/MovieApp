b890a15a772bfb7d7446cfa60fb5feafe3ce32f8
Keshav <keshavgupta.mnnit@gmail.com>
2024-08-12 14:27:49 +0530

    image loader added

--- app/src/main/java/com/atlys/movieapp/ui/screens/detailscreen/MovieDetailsScreen.kt
+++ app/src/main/java/com/atlys/movieapp/ui/screens/detailscreen/MovieDetailsScreen.kt
@@ -1,24 +1,25 @@
 package com.atlys.movieapp.ui.screens.detailscreen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 import com.atlys.movieapp.models.MovieDto
 import com.atlys.movieapp.navigation.MainNavGraph
 import com.atlys.movieapp.ui.components.TopBar
@@ -44,10 +45,15 @@ fun MovieDetailsScreen(navigator: DestinationsNavigator, model: MovieDto) {
 fun MovieDetails(model: MovieDto, paddingValues: PaddingValues) {
     LazyColumn(modifier = Modifier.padding(paddingValues)) {
         item {
-            AsyncImage(
+            SubcomposeAsyncImage(
                 contentScale = ContentScale.FillBounds,
-                model = model.posterPath?.let{"${AppConstants.BASE_IMAGE_URL}$it"},
+                model = model.posterPath?.let { "${AppConstants.BASE_IMAGE_URL}$it" },
                 contentDescription = null,
+                loading = {
+                    Box(contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator(color = Color.Blue)
+                    }
+                },
                 modifier = Modifier
                     .height(360.dp)
                     .padding(all = 16.dp)
--- app/src/main/java/com/atlys/movieapp/ui/screens/homescreen/MovieHomeScreenItem.kt
+++ app/src/main/java/com/atlys/movieapp/ui/screens/homescreen/MovieHomeScreenItem.kt
@@ -1,22 +1,26 @@
 package com.atlys.movieapp.ui.screens.homescreen
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 import com.atlys.movieapp.ui.theme.typography
 
 @Composable
@@ -29,10 +33,15 @@ fun MovieHomeScreenItem(imageSrc: String, title: String, onClick: () -> Unit) {
             .padding(4.dp)
             .clickable { onClick() }
     ) {
-        AsyncImage(
+        SubcomposeAsyncImage(
             contentScale = ContentScale.FillBounds,
             model = imageSrc,
             contentDescription = null,
+            loading = {
+                Box(contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator(color = Color.Blue)
+                }
+            },
             modifier = Modifier
                 .fillMaxHeight(0.8f)
                 .fillMaxWidth()
